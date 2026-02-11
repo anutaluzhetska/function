a9ac399f964f345985cddc2e0eebf940b88abd23
Anna Luzhetska <anyluzeckaana.study@gmail.com>
2026-02-11 10:45:28 +0200

    code_function.js

---
+++ code_function.js
@@ -0,0 +1,24 @@
+console.log("functions:")
+
+function simpleSquare(x) {
+    return x * x
+}
+
+function mathSquare(x) {
+    return Math.pow (x, 2)
+}
+
+function simpleCub(x) { 
+    return x * x * x
+}
+
+function mathCub(x) {
+    return Math.pow (x, 3)
+}
+
+console.log("First number square:", simpleSquare(8)) 
+console.log("Second number square:", mathSquare(3)) 
+console.log("First number cub:", simpleCub(8))
+console.log("Second number cub:", mathCub(3))
+
+ 
